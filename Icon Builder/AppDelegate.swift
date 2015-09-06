1ff7df62a5edb3db0db91da29b2170f3b3000bce
Vegard Solheim Theriault <vegard@moonwearables.com>
2015-09-06 17:13:16 +0200

    Initial Commit

---
+++ Icon Builder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Icon Builder
+//
+//  Created by Vegard Solheim Theriault on 06/09/15.
+//  Copyright © 2015 Vegard Solheim Theriault. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Icon Builder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Icon Builder
+//
+//  Created by Vegard Solheim Theriault on 06/09/15.
+//  Copyright © 2015 Vegard Solheim Theriault. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
